c242a607eb1df3fea3f7660919e6283b474d9042
chorusfruit <564005180@qq.com>
2026-05-31 17:25:54 +0800

    fix: repair widget test with proper provider setup

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,35 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
 
 import 'package:oboeru_flutter/app.dart';
+import 'package:oboeru_flutter/providers/vocabulary_provider.dart';
+import 'package:oboeru_flutter/providers/learning_provider.dart';
+import 'package:oboeru_flutter/providers/quiz_provider.dart';
+import 'package:oboeru_flutter/providers/favorites_provider.dart';
+import 'package:oboeru_flutter/providers/settings_provider.dart';
+import 'package:oboeru_flutter/providers/ai_provider.dart';
+import 'package:oboeru_flutter/providers/tts_provider.dart';
+
+Widget _wrapWithProviders(Widget child) {
+  return MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => VocabularyProvider()),
+      ChangeNotifierProvider(create: (_) => LearningProvider()),
+      ChangeNotifierProvider(create: (_) => QuizProvider()),
+      ChangeNotifierProvider(create: (_) => FavoritesProvider()),
+      ChangeNotifierProvider(create: (_) => SettingsProvider()),
+      ChangeNotifierProvider(create: (_) => AIProvider()),
+      ChangeNotifierProvider(create: (_) => TTSProvider()),
+    ],
+    child: child,
+  );
+}
 
 void main() {
   testWidgets('App loads without error', (WidgetTester tester) async {
-    await tester.pumpWidget(const OboeruApp());
-    expect(find.text('Oboeru'), findsOneWidget);
+    await tester.pumpWidget(_wrapWithProviders(const OboeruApp()));
+    await tester.pump();
+    expect(find.text('Oboeru'), findsWidgets);
   });
 }
